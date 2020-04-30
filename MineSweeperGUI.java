a527c3122c94c9a89bd6863d7c83db979d53d44d
xCito <juanlantiguajr@gmail.com>
2020-04-30 00:45:50 -0400

    Added button

--- MineSweeperGUI.java
+++ MineSweeperGUI.java
@@ -1,10 +1,18 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class MineSweeperGUI {
     public static void main(String[] args) {
         System.out.println("Hello World");
         JFrame appFrame = new JFrame();
 
+        JButton button = new JButton("Click Here");  
+        int xPos = 50, yPos = 100, wid = 100, hgt = 40;
+        button.setBounds(xPos,yPos,wid,hgt);  
+
+        appFrame.setLayout(null);
+
+        appFrame.add(button);  
         // Set the frame's width (400) and height (250) in pixels
         appFrame.setSize(400, 250);
         
