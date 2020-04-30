6a01f2e089f48613f53ca26224c50afbaf5b0fc2
xCito <juanlantiguajr@gmail.com>
2020-04-30 00:17:30 -0400

    commiting the gui file

---
+++ MineSweeperGUI.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+public class MineSweeperGUI {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        JFrame appFrame = new JFrame();
+
+        // Set the frame's width (400) and height (250) in pixels
+        appFrame.setSize(400, 250);
+        
+        // Set the frame's title
+        appFrame.setTitle("An Empty Frame");
+        
+        // Set the program to exit when the user
+        // closes the frame
+        appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Make the frame visible to the user
+        appFrame.setVisible(true);
+
+    }
+}
