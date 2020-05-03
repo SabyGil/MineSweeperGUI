00ea2d73fc0fcdd2b53aadf99d166af8367cec87
SabyGil <sabygil@yahoo.com>
2020-05-02 22:58:47 -0400

    added grid class base code

---
+++ Grid.java
@@ -0,0 +1,68 @@
+public class Grid {
+    private boolean bombGrid[][];
+    private int [][];
+    private int numRows;
+    private int numColumns;
+    private int numBombs;
+
+    public Grid(){
+    //10 rows, 10 columns, (10 x 10 Grid), 25 bombs
+        this.numRows = 10;
+        this.numColumns = 10;
+        this.numBombs = 25;
+    }
+        
+
+    public Grid(int rows, int columns){
+        this.numRows = rows;
+        this.numColumns = columns;
+        this.numBombs = 25;
+
+    } 	
+    			//(rows x columns Grid), default value of 25 bombs 
+    public Grid(int rows, int columns, int numBombs){
+        this.numRows = rows;
+        this.numColumns = columns;
+        this.numBombs = numBombs;
+
+    } 		//(rows x columns Grid), numBombs as specified
+
+    public int getNumRows (){
+
+    } 
+
+    public int getNumColumns(){
+
+    }
+
+    public int getNumbBombs(){
+
+    }
+
+    public boolean getBombGrid(){
+
+    }
+
+    public int getCountGrid(){
+
+    }
+
+    public boolean isBombAtLocation(int row, int column){
+        
+    }
+
+    public int getCountAtLocation(int row, int column){
+
+    }
+
+    public void createBombGrid(){
+        //called by constructors to create and populate the bombGrid
+
+    }
+
+    public void createCountGrid(){
+
+        //call
+    }
+
+}
--- MineSweeperGUI.java
+++ MineSweeperGUI.java
@@ -1,20 +1,22 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class MineSweeperGUI {
+public class MineSweeper_GUI {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
         JFrame appFrame = new JFrame();
 
         // Set the frame's width (400) and height (250) in pixels
         appFrame.setSize(400, 250);
-        
+
         // Set the frame's title
         appFrame.setTitle("An Empty Frame");
-        
+
         // Set the program to exit when the user
         // closes the frame
         appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         // Make the frame visible to the user
         appFrame.setVisible(true);
 
